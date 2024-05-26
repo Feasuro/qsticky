581f8282dee75e282dc4e11f90cf73702ddef615
Feasuro <feasuro@protonmail.com>
2024-05-26 20:29:38 +0200

    Drag'n'drop support

--- qsticky.py
+++ qsticky.py
@@ -4,7 +4,7 @@ import sys
 
 from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction, QIcon
-from PyQt6.QtWidgets import QApplication, QTextEdit, QPushButton, QGridLayout
+from PyQt6.QtWidgets import QApplication, QTextEdit
 
 import resources
 
@@ -14,14 +14,19 @@ class NoteWindow(QTextEdit):
     
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
-        #self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
-
+        # Window decorations
+        self.setWindowTitle('QSticky')
+        self.setWindowIcon(QIcon(':/icons/main'))
+        self.setToolTip('''Drag with left mouse button.\nRight click to open context menu.''')
+        self.setToolTipDuration(2000)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
+        # Icons
         self.icons = {}
         self.icons['new'] = QIcon(':/icons/new')
         self.icons['hide'] = QIcon(':/icons/hide')
         self.icons['properties'] = QIcon(':/icons/prop')
         self.icons['delete'] = QIcon(':/icons/del')
+        # Actions
         self.actions = {}
         self.actions['new'] = QAction('&New', self)
         self.actions['new'].setShortcut('Ctrl+N')
@@ -31,7 +36,6 @@ class NoteWindow(QTextEdit):
         self.actions['properties'].setShortcut('Ctrl+O')
         self.actions['delete'] = QAction('&Delete', self)
         self.actions['delete'].setShortcut('Ctrl+D')
-
         for key in self.actions:
             self.actions[key].setIcon(self.icons[key])
 
@@ -42,6 +46,17 @@ class NoteWindow(QTextEdit):
         menu.insertActions(top, self.actions.values())
         menu.insertSeparator(top)
         menu.exec(event.globalPos())
+
+    def mousePressEvent(self, event) -> None:
+        """ Drag & drop support - save starting position """
+        super().mousePressEvent(event)
+        if event.button() == Qt.MouseButton.LeftButton:
+            self._dragStart = event.pos()
+
+    def mouseMoveEvent(self, event) -> None:
+        """ Drag & drop support - move window"""
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            self.move(event.globalPosition().toPoint() - self._dragStart)
 
 
 if __name__ == '__main__':
